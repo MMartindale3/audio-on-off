cae3db96809df9b8b1a2115476e1135f8ac00e7b
MMartindale3 <145407645+MMartindale3@users.noreplay.github.com>
2024-01-30 19:46:07 -0700

    commiting notes.js

--- docs/notes.js
+++ docs/notes.js
@@ -1,1 +1,50 @@
+/* 
                  Conditional Logic 
+ 
+                 conditional statement
+                 question==> true or false
+                
+                Type 1 If
+                 if(operand/value operator operand/value){
+
+                 }
+
+                Type 2 If else
+                 if(operand/value operator operand/value){
+                     true block
+                 }else{
+                     false block
+                 }
+
+
+                 Equality Opertors
+                 =
+                 ==   
+                 ===
+
+                 Let variables allow you change variable value and data
+                 Const variable have a fixed value
+
+              
+ 
+
+*/
+
+/* 
+                   State Variables the User Interface
+                   isPlaying = false audio is not playing
+                   
+                   stateVariable 
+                   developers way of assigning a value 
+                   to represent the state of UI environment..
+
+*/
+
+/* 
+         Creating A Toggle 
+         
+    1.  Create an if else block 
+    2.  Check the value of the state variable.
+    3. True block make the ui change & update the state variable
+    4. False block make the ui changes & update the state variable
+*/
